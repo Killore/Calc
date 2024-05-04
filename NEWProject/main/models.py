65581d350d0ca47512a402aff3c71d85faf0852f
Killore <gaiduk_kirill@mail.ru>
2024-05-04 19:14:10 +0300

    DZ Five

--- NEWProject/main/models.py
+++ NEWProject/main/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField("Название", max_length=50)
     task = models.TextField('Описание')
-    data = models.DateField('Дата')
-    time = models.TimeField('Время')
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
