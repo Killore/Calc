a31c3a6b5f21f8dc2e1741633254cd68c896985b
Killore <gaiduk_kirill@mail.ru>
2024-05-04 16:08:01 +0300

    DZ Third

--- NEWProject/main/views.py
+++ NEWProject/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, "main/1SHABLON.html")
+    return render(request, "main/index.html")
 
 def about(request):
-    return HttpResponse("<h1>About</h1>")
+    return render(request, "main/1SHABLON.html")
