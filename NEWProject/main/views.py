389dd9fe829dbe9abfbc19a0f62b6bd46ebfc7f3
Killore <gaiduk_kirill@mail.ru>
2024-05-05 15:49:38 +0300

    DZ SIX

--- NEWProject/main/views.py
+++ NEWProject/main/views.py
@@ -5,8 +5,14 @@ from django.http import HttpResponse
 from .models import Task
 
 def index(request):
-    task = Task.objects.all()
+    task = Task.objects.order_by('-id')
     return render(request, "main/index.html", {'title': 'Главная страница', 'tasks': task})
 
 def about(request):
     return render(request, "main/about.html")
+
+def create(request):
+    return render(request, "main/create.html")
+
+def shablon(request):
+    return render(request, "main/1SHABLON.html")
