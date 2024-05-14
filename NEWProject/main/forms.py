e574a9f39c12d8000c6e6a4079fbf4aabd87c534
Killore <gaiduk_kirill@mail.ru>
2024-05-14 19:53:23 +0300

    DZ Eight

--- NEWProject/main/forms.py
+++ NEWProject/main/forms.py
@@ -1,10 +1,12 @@
 from .models import Task, Shablon
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets, TextInput
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'task']
+        widgets = {"title": TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите задание'}),
+                   "task": TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите задание'})}
 
 class ShablonForm(ModelForm):
     class Meta:
